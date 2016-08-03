40e19f8addd85dc096bf812d46ada2be3d9d4f5c
Joe Nellis <joe@jnellis.net>
2016-08-02 17:55:30 -0700

    Remove flush on readComplete to reduce flood to browser.

--- src/main/java/com/goodgamenow/source/serverquery/websocketserver/WebSocketHandler.java
+++ src/main/java/com/goodgamenow/source/serverquery/websocketserver/WebSocketHandler.java
@@ -84,12 +84,6 @@ public class WebSocketHandler
 
   }
 
-
-  @Override
-  public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-    ctx.flush();
-  }
-
   @Override
   public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
     cause.printStackTrace();
